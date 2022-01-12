1c2678c5cbf9b7804627138ee0cb49002b45d111
Jeffrey <j.vos@catawiki.nl>
2022-01-12 22:20:23 +0200

    added charity function and moved keyboard input to function

--- main.py
+++ main.py
@@ -23,23 +23,36 @@ class Game:
             self.current_location = dice + self.current_location
         return self.current_location
     
+    def calculate_tax(self):
+        tax_percentage = self.game_actions['tax']
+        tax_amount = self.current_player.calculate_payday() * tax_percentage / 100
+        print(f"You need to pay $ {tax_amount} tax")
+        return tax_amount
+
+    def calculate_charity(self):
+        charity_percentage = self.game_actions['charity']
+        payday = self.current_player.calculate_payday()
+        charity_amount = payday * charity_percentage / 100
+        return charity_amount
+
     def check_game(self):
         game_status = self.current_player.check_game()
         if game_status:
             print('You won the game')
             quit()
         return game_status
+    
+    def check_keyboard_input(self, msg):
+        keyboard_input = input(msg)
+        if keyboard_input.upper() == "Y" or keyboard_input == "":
+            return True
+        else:
+            return False
 
     def payday(self):
         current_pay = self.current_player.calculate_payday()
         self.current_player.payment(current_pay)
         return current_pay
-    
-    def calculate_tax(self):
-        tax_percentage = self.game_actions['tax']
-        tax_amount = self.current_player.calculate_payday() * tax_percentage / 100
-        print(f"You need to pay $ {tax_amount} tax")
-        return tax_amount
 
     def output_money(self):
         money = self.current_player.get_player()["finances"]["cash"]
@@ -73,13 +86,17 @@ class Game:
     def play(self):
         while True:
             self.check_game()
-            throw_dice = input("Throw dice? Y / N ")
+            
             self.output_stats()
-            if throw_dice.upper() == "Y" or throw_dice == "":
+            if self.check_keyboard_input("Throw dice? Y / N "):
                 self.dice()
                 board = self.game_board[str(self.current_location)]
+                print(board["name"])
                 if board['name'] == "Tax":
                     self.deduct_tax()
+                elif board["name"] == "Charity":
+                    charity_amount = self.calculate_charity()
+                    self.current_player.deduct_amount(charity_amount)
             else:
                 quit()
 
